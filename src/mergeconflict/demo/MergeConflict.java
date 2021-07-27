81e776a18db005ce2e5bb17f928b7c95c6603b41
VishMaster17 <vishnu.h.s007@gmail.com>
2021-07-27 17:15:37 +0530

    feature branch 5

--- src/mergeconflict/demo/MergeConflict.java
+++ src/mergeconflict/demo/MergeConflict.java
@@ -5,7 +5,7 @@ public class MergeConflict {
 	public static void main(String[] args) {
 		System.out.println("Mergeconflict line change");
 		System.out.println("This is master branch 3"); 
-		System.out.println("This is feature branch 4"); 
+		System.out.println("This is feature branch 5"); 
 	}
 
 }
