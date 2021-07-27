c3ac5e2056e2b551ac6d76367ef5a7ca1135889e
VishMaster17 <vishnu.h.s007@gmail.com>
2021-07-27 11:18:22 +0530

    Changed feature line

--- src/mergeconflict/demo/MergeConflict.java
+++ src/mergeconflict/demo/MergeConflict.java
@@ -4,7 +4,7 @@ public class MergeConflict {
 	
 	public static void main(String[] args) {
 		System.out.println("Mergeconflict line change");
-		System.out.println("This is feature branch");
+		System.out.println("This is feature branch 3");
 	}
 
 }
